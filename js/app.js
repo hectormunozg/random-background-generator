4b276ea28cea3fe59bbf82d1bdf049fd4d5790ad
hectormunozg <hectormunozgarcia@gmail.com>
2015-06-23 14:39:58 +0100

    basic functions

--- js/app.js
+++ js/app.js
@@ -2,4 +2,18 @@
 var colors = [];
 var color = 0;
 
+// generate color function
+var generateColor = function() {
+	color = "#" + (Math.floor(Math.random() * 1000000));
+}
+
+// check if current color is in array
+var checkColor = function() {
+	while(colors.indexOf(color) !== -1){
+		generateColor();
+	}
+}
+
+
+
 
